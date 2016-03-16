4cb8f89cd4808db056eb37a4c88e382c520cf267
Quentin WEPHRE <wephr001@cougars.csusm.edu>
2016-03-16 10:51:21 -0700

    Share button added to the action bar.

--- app/src/main/java/com/example/abel/quizapp/QuestionList.java
+++ app/src/main/java/com/example/abel/quizapp/QuestionList.java
@@ -3,11 +3,15 @@ package com.example.abel.quizapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class QuestionList extends AppCompatActivity {
@@ -32,6 +36,32 @@ public class QuestionList extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                //Toast.makeText(getApplicationContext(), "SHARE MENU BUTTON", Toast.LENGTH_LONG).show();
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Hey! Check my new Quizz score: " + this.score);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(), "SETTINGS MENU BUTTON", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
